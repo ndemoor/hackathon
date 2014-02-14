67eaeacc939076491bfb831173d160f3a85467cb
Christophe Hansen <hansen.christophe@gmail.com>
2014-02-14 15:02:59 +0100

    Slicing last scans page & polish home

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,28 @@
 $(function(){
+
+	// Call FastClick
 	window.addEventListener('load', function() {
     	new FastClick(document.body);
 	}, false);
+
+	// Call foundation accordeon
+	$(document).foundation({
+        tab: {
+            callback: function (tab){
+                console.log(tab);
+                console.log('coucou');
+            }
+        }
+  	});
+	// Calcul height of Left-col
+	calculateHeight();	
+
 });
+
+function calculateHeight(){
+	$('.left-col').each(function(){
+		var rowHeight = $(this).parent().height();
+		$(this).css('height' , rowHeight+'px');
+		$(this).find('i.fa').css('margin-top' , (rowHeight-15)/2 +'px');
+	})
+}
